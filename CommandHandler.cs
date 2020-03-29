c490ec4cc3c8d34bc7ea959a68948a1e6c2bde8c
Reesha Rajen <rajen.reese@gmail.com>
2020-03-28 22:56:42 -0700

    Add files via upload

---
+++ CommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ada
+{
+    public class CommandHandler
+    {
+        private DiscordSocketClient _client;
+
+        private CommandService _service;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _service = new CommandService();
+
+            _service.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommandAsync;
+
+        }
+
+        private async Task HandleCommandAsync(SocketMessage socketMessage)
+        {
+            var msg = socketMessage as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+
+            int argPos = 0;
+
+            if(msg.HasCharPrefix('!', ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+
+                if(!result.IsSuccess)
+                {
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Ada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new Program().StartAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            Console.WriteLine("Bot is starting up. (2018, by Reesha Rajen) v. 2.1");
+            Console.WriteLine("Success!");
+
+           _client = new DiscordSocketClient();
+
+            //new CommandHandler(_client); //this was creating an extra bot.
+
+            await _client.LoginAsync(TokenType.Bot, "NDE1MTA1Njc4OTc0Mzg2MTc3.DXPqWQ.XzhbA8eAK4pg1c5-Yz0rhcDwenc"); // nice token :p
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            await Task.Delay(-1);
+        }
+    }
+}
